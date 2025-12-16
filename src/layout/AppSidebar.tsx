f080c1f5c403b4836184a8a9924dcaed19bcc751
heartsantor <antorboss.bd@gmail.com>
2025-12-17 00:15:43 +0600

    Merge branch 'dev' of https://github.com/trialvo/graduate_shop_admin

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import { Link, useLocation } from "react-router";
+import { Link, useLocation } from "react-router-dom";
 
 import {
   LayoutDashboard,
