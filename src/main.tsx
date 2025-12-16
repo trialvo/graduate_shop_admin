399e69ba65c0e4830bc1839c198826d6e0a162d7
heartsantor <antorboss.bd@gmail.com>
2025-12-16 21:22:38 +0600

    added tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,34 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
+
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { ThemeProvider } from "./context/ThemeContext.tsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <AppWrapper>
-        <App />
-      </AppWrapper>
+      <QueryClientProvider client={queryClient}>
+        <AppWrapper>
+          <App />
+        </AppWrapper>
+
+        {/* Remove in production if you prefer */}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
 );
