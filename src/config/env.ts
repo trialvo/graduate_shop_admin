cd81ad03e67388db120da584756cf1f9c56485d6
heartsantor <antorboss.bd@gmail.com>
2026-01-05 22:03:42 +0600

    add

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,17 +2,12 @@
 //   import.meta.env.VITE_API_ORIGIN || "https://shop-api.shoplinkbd.com";
 //   export const PUBLIC_ORIGIN = import.meta.env.VITE_PUBLIC_ORIGIN || "https://shop-api.shoplinkbd.com";
 
-export const API_ORIGIN =
-  import.meta.env.VITE_API_ORIGIN || "https://api.graduatefashionbd.com";
-  export const PUBLIC_ORIGIN = import.meta.env.VITE_PUBLIC_ORIGIN || "https://api.graduatefashionbd.com";
-
+export const API_ORIGIN = "https://api.graduatefashionbd.com";
+export const PUBLIC_ORIGIN = "https://api.graduatefashionbd.com";
 
 export const API_PREFIX = import.meta.env.VITE_API_PREFIX || "/api/v1";
 
 export const API_BASE_URL = `${API_ORIGIN}${API_PREFIX}`;
-
-
-
 
 export function toPublicUrl(path?: string | null): string | null {
   if (!path) return null;
