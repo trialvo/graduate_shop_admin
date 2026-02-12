c4f4369ce625510f6a42eae83016c861930d85a5
heartsantor <antorboss.bd@gmail.com>
2026-02-12 15:53:15 +0600

    add

--- src/components/dashboard/OrderStatusGrid.tsx
+++ src/components/dashboard/OrderStatusGrid.tsx
@@ -3,7 +3,7 @@ import { dashboardStatusData } from "../../pages/Dashboard/dashboardStatusData";
 
 const OrderStatusGrid = () => {
   return (
-    <div className="grid h-full grid-cols-2 gap-4 content-stretch">
+    <div className="grid h-full grid-cols-2 gap-4 content-stretch w-full">
       {dashboardStatusData.map((item) => (
         <OrderStatusCard key={item.id} item={item} />
       ))}
