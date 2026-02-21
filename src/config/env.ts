cb533461526004de3eebfd86c133189df27e115f
Arafat Shovo <5arafatshovo@gmail.com>
2026-02-21 14:58:33 +0600

    chore: env update

--- src/config/env.ts
+++ src/config/env.ts
@@ -16,14 +16,15 @@
 
 
 //this is thhe main
-export const API_ORIGIN = "https://api.graduatefashionbd.com";
-export const PUBLIC_ORIGIN = "https://api.graduatefashionbd.com";
+// export const API_ORIGIN = "https://api.graduatefashionbd.com";
+// export const PUBLIC_ORIGIN = "https://api.graduatefashionbd.com";
 
 
-// export const API_ORIGIN =
-//   "https://graduatefashion-api-641431966702.asia-southeast1.run.app";
-// export const PUBLIC_ORIGIN =
-//   "https://graduatefashion-api-641431966702.asia-southeast1.run.app";
+ export const API_ORIGIN =
+  "https://graduatefashion-api-641431966702.asia-south1.run.app";
+  
+export const PUBLIC_ORIGIN =
+  "https://graduatefashion-api-641431966702.asia-south1.run.app";
 
 export const API_PREFIX = import.meta.env.VITE_API_PREFIX || "/api/v1";
 
